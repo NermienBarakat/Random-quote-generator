34c367fb816b3eef9092af14dd835ccc7281a404
Nermien Barakat <nermienbarakat@gmail.com>
2020-01-21 23:46:23 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# a_random_quote_generator-v1
+ Random Quotes
---
+++ js/script.js
@@ -0,0 +1,44 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+var quoteSource=[
+    {
+      quote:"Atheism is a non-prophet organization.",
+      name:"George Carlin"
+    },
+    {
+      quote:"Hapiness is not something ready made. It comes from your own actions.",
+      name:"Dalai Lama"
+    }
+
+];
+  
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
