c90fddbc592522ac7944eddc9489d0a9157ed547
Nermien Barakat <nermienbarakat@gmail.com>
2020-01-25 01:27:55 +0000

    HTML Edit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,123 @@
 /******************************************
-Nermien barakat
-Click the link below to view on CodePen
-https://codepen.io/NermienBarakat/full/wvBRZxj
-project 1 - A Random Quote Generator
-// I am going after Exceeds Expectations. Thank you 
+Nermien barakat -- nermien@treenodes.com
+A Random Quote Generator
 ******************************************/
 
 //`quotes` array 
 const quotes = [
   {
-    quote: "Atheism is a non-prophet organization.",
+    quote: "Do. Or do not. There is no try.",
+    source: "Yoda",
+    year: 1980,
+    citation: "Star Wars: Episode V - The Empire Strikes Back",
+    tags: ["motivating"]
+  },
+  {
+    quote: "Your focus determines your reality.",
+    source: "Qui-Gon Jinn",
+    year: 1999,
+    citation: "Star Wars: Episode I - The Phantom Menace",
+    tags: ["inspiring", "motivating"]
+  },
+  {
+    quote: "In my experience there is no such thing as luck.",
+    source: "Obi-Wan Kenobi",
+    year: 1977,
+    citation: "Star Wars: Episode IV - A New Hope"
+  },
+  {
+    quote: "The greatest teacher, failure is.",
+    source: "Yoda",
+    year: 2017,
+    citation: "Star Wars: Episode VIII - The Last Jedi",
+    tags: ["motivating", "inspiring", "funny"]
+  },
+  {
+    quote: "When 900 years old, you reach… Look as good, you will not.",
+    source: "Yoda",
+    year: 1983,
+    citation: "Star Wars: Episode VI - Return Of The Jedi",
+    tags: ["funny", "impossible"]
+  },
+  {
+    quote: "I've got a bad feeling about this.",
+    source: "Han Solo"
+  },
+  {
+    quote: "We must learn to live together as brothers, or perish together as fools.",
+    source: "Martin Luther King, Jr."
+  },
+  {
+    quote: "You don't learn to walk by following the rules. You learn by doing, and by falling over.",
+    source: "Richard Branson"
+  },
+  {
+    quote: "Anyone who stops learning is old, whether at twenty or eighty. Anyone who keeps learning stays young. The greatest thing in life is to keep your mind young.",
+    source: "Henry Ford"
+  },
+  {
+    quote: "He who is not everyday conquering some fear has not learned the secret of life.",
+    source: "Ralph Waldo Emerson"
+  },
+  {
+    quote: "This is the most simple and basic component of life: our struggles determine our successes.",
+    source: "Mark Manson"
+  },
+  {
+    quote: "There is no innovation and creativity without failure. Period.",
+    source: "Brené Brown"
+  },
+  {
+    quote: "If more information was the answer, then we’d all be billionaires with perfect abs.",
+    source: "Derek Sivers"
+  },
+  {
+    quote: "Luck is what happens when preparation meets opportunity.",
+    source: "Seneca"
+  },
+  {
+    quote: "We suffer more often in imagination than in reality.",
+    source: "Seneca"
+  },
+  {
+    quote: "If a man knows not to which port he sails, no wind is favorable.",
+    source: "Seneca"
+  },
+  {
+    quote: "You are the average of the five people you spend the most time with",
+    source: "Jim Rohn"
+  },
+  {
+    quote: "Discipline is the bridge between goals and accomplishment",
+    source: "Jim Rohn"
+  },
+  {
+    quote: "We must all suffer one of two things: the pain of discipline or the pain of regret or disappointment",
+    source: "Jim Rohn"
+  },
+  {
+    quote: "Happiness is not having what you want. Happiness is wanting what you have",
+    source: "Anonymous"
+  },
+  {
+    quote: "Develop courage by doing courageous things",
+    source: "Gary Tuerack"
+  },
+  {
+    quote: "If you do not change direction, you may end up where you are heading",
+    source: "Lao Tzu"
+  },
+  {
+    quote: "Right feelings follow right actions",
+    source: "Anonymous"
+  },
+  {
+    quote: "Atheism is a non-prophet organization",
     source: "George Carlin",
     tag: "Fun"
   },
   {
-    quote: "Hapiness is not something ready made. It comes from your own actions.",
+    quote: "Hapiness is not something ready made. It comes from your own actions",
     source: "Dalai Lama"
   },
   {
@@ -25,14 +128,14 @@ const quotes = [
     tag: "Wise"
   },
   {
-    quote: "Understanding is the first step to acceptance, and only with acceptance can there be recovery.",
+    quote: "Understanding is the first step to acceptance, and only with acceptance can there be recovery",
     source: "J.K. Rowling",
     citation: "Harry Potter",
     year: "2000",
     tag: "Acceptance"
   },
   {
-    quote: "It's the questions we can't answer that teach us the most. They teach us how to think. If you give a man an answer, all he gains is a little fact. But give him a question and he'll look for his own answers.",
+    quote: "It's the questions we can't answer that teach us the most. They teach us how to think. If you give a man an answer, all he gains is a little fact. But give him a question and he'll look for his own answers",
     source: "Patrick Rothfuss",
     citation: "The Wise Man's Fear",
     year: "2011",
